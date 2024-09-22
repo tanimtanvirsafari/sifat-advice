da57b01fa7c6211e65463d9836c021fb578b2e84
tanimtanvirsafari <tanimtanvirsafari@gmail.com>
2024-09-22 12:49:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the form from submitting normally
+    alert('Submit Successfully');
+});
